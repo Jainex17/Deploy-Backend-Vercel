dff2360a11125aebd3a0593c91f7b01723047a3b
jainex17 <jainexp017@gmail.com>
2023-10-03 00:20:24 +0530

    connect backend to mongodb database

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
 app.use(cors({
     origin: 'https://deploy-vercel-inky.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -10,6 +13,23 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then((data)=>{
+            console.log(`Mongodb connected with server: ${data.connection.host}`);
+        })
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1);
+    }
+}
+
+dbConnect();
 
 const userRoute = require('./routes/userRoute');
 app.use('/api/v1', userRoute);
